cf1314781d068db916480d6d4d9c6774919ebbd3
NicholasNeto <nicholasneto@outlook.com>
2020-02-25 14:39:05 -0300

    created of routes

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+
+module.exports = app => {
+    app.get("/", (request, response) => {
+        response.json({ status: "NTask API" })
+    })
+}
---
+++ routes/tasks.js
@@ -0,0 +1,10 @@
+
+module.exports = app => {
+    const Tasks = app.models.tasks;
+
+    app.get("/tasks", (request, response) => {
+        Tasks.findAll({}, (tasks) => {
+            response.json({ tasks: tasks })
+        })
+    });
+}
