3feba95efb8bcd4fa0431b9aed041cfbb73cb721
NicholasNeto <nicholasneto@outlook.com>
2020-04-27 18:44:00 -0300

    created new structure for routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,9 +2,39 @@
 module.exports = app => {
     const Tasks = app.db.models.Tasks;
 
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}).then(tasks => {
-            res.json({ tasks: tasks})
+    app.route("/tasks")
+        .all((req, res, next) => {
+            // Middleware de pré-execução das rotas
+            if (req.body) {
+                delete req.body.id;
+            }
+            next()
         })
-    });
+        .get((req, res) => {
+            // "/tasks" : Lista tarefas
+            Tasks.findAll({})
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message })
+                })
+        })
+        .post((req, res) => {
+            // "/tasks" : Cadastra uma nova tarefa
+        });
+
+    app.route("/tasks/:id")
+        .all((req, res) => {
+            // Middleware de pré-execução das rotas
+            delete req.body.id
+            next()
+        })
+        .get((req, res) => {
+            // "/tasks/1" : Consulta uma tarefa
+        })
+        .put((req, res) => {
+            // "/tasks/1" : Atualiza uma tarefa
+        })
+        .delete((req, res) => {
+            // "/tasks/1" : Exclui uma tarefa
+        });
 }
