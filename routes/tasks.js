1d857fc3e00e922d87003d9485b29f89b11ba37a
NicholasNeto <nicholasneto@outlook.com>
2020-05-05 17:23:27 -0300

    implaments function for PUT

--- routes/tasks.js
+++ routes/tasks.js
@@ -25,14 +25,32 @@ module.exports = app => {
     app.route("/tasks/:id")
         .all((req, res) => {
             // Middleware de pré-execução das rotas
-            delete req.body.id
+            if (req.body) {
+                delete req.body.id;
+            }
             next()
         })
         .get((req, res) => {
             // "/tasks/1" : Consulta uma tarefa
+            Tasks.findOne({ where: req.params })
+                .then(result => {
+                    if (result) {
+                        res.json(result)
+                    } else {
+                        res.sendStatus(404)
+                    }
+                })
+                .catch(error => {
+                    res.status(412).json({ msg: error.message })
+                })
         })
         .put((req, res) => {
             // "/tasks/1" : Atualiza uma tarefa
+            Tasks.update(req.body, { where: req.params })
+                .then(result => { res.sendStatus(204) })
+                .catch(error => {
+                    res.status(412).json({ msg: error.message })
+                })
         })
         .delete((req, res) => {
             // "/tasks/1" : Exclui uma tarefa
